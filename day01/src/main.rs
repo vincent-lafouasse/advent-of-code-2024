2e71bb15aa434907b1e489252750750b67bada6a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-12-01 20:29:26 +0100

    remove unnecessary static lifetime indicator

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -3,9 +3,9 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
 #[allow(unused)]
-const INPUT_PATH: &'static str = "src/input1.txt";
+const INPUT_PATH: &str = "src/input1.txt";
 #[allow(unused)]
-const TEST_INPUT_PATH: &'static str = "src/small.txt";
+const TEST_INPUT_PATH: &str = "src/small.txt";
 
 fn main() -> io::Result<()> {
     println!("{}", solve_part2());
