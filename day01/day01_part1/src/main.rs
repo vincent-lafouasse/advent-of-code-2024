1acb27bfe5425b71f34327957f46683e79038059
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-12-01 18:04:18 +0100

    init

--- day01/day01_part1/src/main.rs
+++ day01/day01_part1/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, BufReader, BufRead};
+
+const INPUT_PATH: &'static str = "input1.txt";
+
+fn main() -> io::Result<()> {
+    let file = File::open(INPUT_PATH)?;
+    let reader = BufReader::new(file);
+
+    let mut list1: Vec<u32> = Vec::new();
+    let mut list2: Vec<u32> = Vec::new();
+
+    for line in reader.lines() {
+    }
+
+    Ok(())
+}
+
+enum ParseError {
+    NotANumber,
+    InvalidLen,
+}
+
+fn parse_line(line: &str) -> Result<(u32, u32), ParseError> {
+    Ok((0, 0))
 }
