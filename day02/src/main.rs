f1bdc8f1f0ab6ad1c7e2bb35b5cb11342dd0d7d7
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-12-03 20:04:18 +0100

    solve part 1

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -11,18 +11,23 @@ fn main() {
     let file = File::open(__TEST_INPUT_PATH).expect("Failed to load input file");
     let reader = BufReader::new(file);
 
+    let mut n_safe = 0;
+
     for line in reader.lines() {
         let line: String = line.expect("Failed to read line from file");
-        dbg!(&line);
         let level: Vec<i32> = parse_line(&line);
         let differences: Vec<i32> = level
             .windows(2)
             .map(|window| window[1] - window[0])
             .collect();
-        let is_safe: bool = differences.iter().all(|x| *x > 0 && *x < 4)
-            || differences.iter().all(|x| *x > -4 && *x < 0);
-        dbg!(&is_safe);
+        if differences.iter().all(|x| *x > 0 && *x < 4)
+            || differences.iter().all(|x| *x > -4 && *x < 0)
+        {
+            n_safe += 1;
+        }
     }
+
+    println!("{n_safe}");
 }
 
 fn parse_line(line: &str) -> Vec<i32> {
